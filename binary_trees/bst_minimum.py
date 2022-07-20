65b2d995e420cc7a8f93207ead43762acc44d4fa
Contrigra <gaponov60b@gmail.com>
2022-07-20 13:56:42 +0300

    a few trees: done

--- binary_trees/bst_minimum.py
+++ binary_trees/bst_minimum.py
@@ -1,6 +1,22 @@
-class Node:
-    def __init__(self, key):
-        self.left = None
-        self.right = None
-        self.val = key
+from __future__ import annotations
+from typing import Optional
 
+
+class Node:
+    def __init__(self, value: int, left: Optional[Node] = None,
+                 right: Optional[Node] = None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+def find_minumum(x):
+    while x.left is not None:
+        x = x.left
+
+    return x, x.value
+
+
+
+root = Node(666, Node(444), Node(555))
+
+print(find_minumum(root))
